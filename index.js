55120ae66a44991ff3c01fbbc8b6ec0da75deeaa
Andreia de Moraes <andreiadmrs@gmail.com>
2020-10-16 10:16:04 -0300

    Projeto feito durante o curso JavaScript: Interfaces e Herança em Orientação a Objetos. Continuamos o projeto do curso passado de JavaScript e aprendemos sobre herança, classes e métodos abstratos, DuckTyping e aprofundamos nos conhecimentos em Orientação a objetos.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
-const cliente2 = new Cliente("Alice", 88822233309);
+const diretor = new Diretor("Rodrigo", 10000, 123456789000);
+diretor.cadastrarSenha("123456789");
+const gerente = new Gerente("Ricardo", 5000, 132547845785);
+gerente.cadastrarSenha("123");
+const cliente = new Cliente("Lais", 78978978934, "456")
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const conta2 = new ContaCorrente(cliente2, 102);
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456789");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
 
-contaCorrenteRicardo.depositar(500);
-let valor = 200;
-contaCorrenteRicardo.tranferir(valor, conta2);
-
-console.log(conta2.saldo);
-
-console.log(contaCorrenteRicardo, cliente2, ContaCorrente.numeroDeContas);
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
